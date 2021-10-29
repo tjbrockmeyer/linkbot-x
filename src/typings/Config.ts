a62314f67a41cbd203d41a9495f1479bbe36d0cc
Tyler Brockmeyer <tjb101@yahoo.com>
2021-10-28 23:16:04 -0500

    Update config to read secret files from docker

--- src/typings/Config.ts
+++ src/typings/Config.ts
@@ -1,36 +1,19 @@
 import { AppConfig } from "aws-sdk"
 
 export type ConfigContent = {
-    parameters: {
-        name: string,
-        path: string,
-        version: number
-    }[],
     databaseArgs: string[]
 }
 
 export type Secrets = {
-    awsAccessKeyId?: string,
-    awsSecretAccessKey?: string,
-}
-
-export type ConfigParameters = {
-    database: {
-        protocol: string,
-        url: string
-    },
-    databaseUser: {
-        user: string,
-        password: string
-    },
-    discordBot: {
-        token: string
-    }
+    dbProtocol: string,
+    dbUrl: string,
+    dbUser: string,
+    dbPassword: string,
+    discordToken: string,
 }
 
 export type Config = {
     content: ConfigContent,
-    parameters: ConfigParameters,
     secrets: Secrets,
 }
 
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,8 @@
-import type {AWSAppConfig, Config, ConfigContent, ConfigParameters, Secrets} from '../typings/Config';
+import type {AWSAppConfig, Config, ConfigContent, Secrets} from '../typings/Config';
 import aws from 'aws-sdk';
 import {promises as fs} from 'fs';
 import {v4 as uuid} from 'uuid';
 
-const ssm = new aws.SSM();
 const appConfig = new aws.AppConfig();
 
 const clientId: string = uuid();
@@ -12,16 +11,24 @@ const cacheLifetime = 2 * 60 * 1000;
 let expirationTime = 0;
 let cachedSecrets: Secrets;
 let cachedConfig: AWSAppConfig;
-let cachedParameters: ConfigParameters;
 
 const readSecrets = async (): Promise<Secrets> => {
-    const {Credentials} = JSON.parse(await fs.readFile('/run/secrets/assume_role', 'utf-8')) as aws.STS.AssumeRoleResponse;
-    const secrets = {
-        awsAccessKeyId: Credentials ? Credentials?.AccessKeyId : cachedSecrets?.awsAccessKeyId,
-        awsSecretAccessKey: Credentials ? Credentials?.SecretAccessKey : cachedSecrets?.awsSecretAccessKey,
+    if(process.env.NODE_ENV !== 'production') {
+        return JSON.parse(await fs.readFile('./config.json', 'utf-8'))
+    }
+    const secretFiles = ['role_creds', 'database_user', 'database', 'discord_creds'];
+    const [roleCreds, dbUser, database, discordCreds] = await Promise.all(
+        secretFiles.map(async file => JSON.parse(await fs.readFile(`/run/secrets/${file}`, 'utf-8'))));
+    const secrets: Secrets = {
+        dbUser: dbUser.user,
+        dbPassword: dbUser.password,
+        dbProtocol: database.protocol,
+        dbUrl: database.url,
+        discordToken: discordCreds.token,
     };
-    process.env.AWS_ACCESS_KEY_ID = secrets.awsAccessKeyId;
-    process.env.AWS_SECRET_ACCESS_KEY = secrets.awsSecretAccessKey;
+    process.env.AWS_ACCESS_KEY_ID = roleCreds?.AccessKeyId;
+    process.env.AWS_SECRET_ACCESS_KEY = roleCreds?.SecretAccessKey;
+    process.env.AWS_SESSION_TOKEN = roleCreds?.SessionToken;
     return secrets;
 }
 
@@ -54,37 +61,15 @@ const requestAppConfig = async (): Promise<AWSAppConfig> => {
     return cachedConfig;
 }
 
-const requestParameters = async (config: ConfigContent): Promise<ConfigParameters> => {
-    if(process.env.NODE_ENV !== 'production') {
-        const contents = await fs.readFile('.config.json', {encoding: 'utf-8'});
-        return JSON.parse(contents).parameters;
-    }
-    const paths = config.parameters.map(p => p.path);
-    const pathToName = new Map(config.parameters.map(p => [p.path, p.name]));
-    const {Parameters} = await ssm.getParameters({Names: paths, WithDecryption: true}).promise();
-    const nameToValue = new Map((Parameters || []).map(p => {
-        try {
-            return [pathToName.get(p.Name as string), JSON.parse(p.Value as string)]
-        } catch {
-            return [pathToName.get(p.Name as string), p.Value as string]
-        }
-    }));
-    const output: Record<string, unknown> = {}
-    config.parameters.forEach(p => output[p.name] = nameToValue.get(p.name));
-    return output as unknown as ConfigParameters
-}
-
 export default async (): Promise<Config> => {
     const now = Number(new Date());
     if(now >= expirationTime) {
         expirationTime = now + cacheLifetime;
         cachedSecrets = await readSecrets();
         cachedConfig = await requestAppConfig();
-        cachedParameters = await requestParameters(cachedConfig.Content as ConfigContent);
     }
     return {
         content: cachedConfig.Content as ConfigContent,
-        parameters: cachedParameters,
         secrets: cachedSecrets,
     }
 }
