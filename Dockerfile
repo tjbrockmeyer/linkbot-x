553f6cd25614b9d29f0dc4107d2f71e61b6c4cb8
Tyler Brockmeyer <tjb101@yahoo.com>
2022-08-02 02:56:03 -0500

    update bootstrap to use vault injector

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,7 @@ RUN npm ci
 RUN npm run build
 ENV NODE_ENV=production
 RUN npm prune
+
+ENV VAULT_ADDR=https://vault.teabee.dev
+ENV VAULT_ROLE_ID=31023f39-1cc5-52bf-3cb1-81b7bc171a66
 CMD npm start
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,29 +1,66 @@
-import nodeVault from 'node-vault';
-import fs from 'fs';
-import path from 'path';
-import os from 'os';
+import nodeVault from "node-vault";
+import fs from "fs/promises";
+import os from "os";
+import path from "path";
+
+const getWrappedToken = async () => {
+  if (process.env.NODE_ENV !== "production") {
+    const vault = nodeVault({
+      token: await fs.readFile(
+        path.join(os.homedir(), ".vault-token"),
+        "utf-8"
+      ),
+      endpoint: process.env.VAULT_ADDR,
+    });
+    const {
+      wrap_info: { token: wrapped_secret_id },
+    } = await vault.write(
+      "auth/approle/role/linkbot-prod/secret-id",
+      {},
+      { headers: { "X-Vault-Wrap-TTL": "30s" } }
+    );
+    return wrapped_secret_id;
+  } else {
+    const file = "/tmp/vault-token";
+    let i = 0;
+    while (true) {
+      try {
+        return await fs.readFile(file, "utf-8");
+      } catch (error) {
+        if (
+          error instanceof Error &&
+          error.message.includes("no such file or directory")
+        ) {
+          i++;
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          if (i > 30) {
+            console.error("timeout exceeded while waiting for the vault token");
+            process.exit(1);
+          }
+        }
+        console.error(error);
+        process.exit(1);
+      }
+    }
+  }
+};
 
 export const bootstrap = async () => {
-    try {
-        if(process.env.VAULT_ROLE_ID === undefined) {
-            const token = fs.readFileSync(path.join(os.homedir(), '.vault-token'), 'utf-8');
-            const vault = nodeVault({token, endpoint: process.env.VAULT_ADDR});
-            const {data: {role_id}} = await vault.read('auth/approle/role/linkbot-prod/role-id');
-            process.env.VAULT_ROLE_ID = role_id;
-        }
-        if(process.env.VAULT_WRAPPED_SECRET_ID === undefined) {
-            const token = fs.readFileSync(path.join(os.homedir(), '.vault-token'), 'utf-8');
-            const vault = nodeVault({token, endpoint: process.env.VAULT_ADDR});
-            const {wrap_info: {token: wrapped_secret_id}} = await vault.write('auth/approle/role/linkbot-prod/secret-id', {}, {headers: {'X-Vault-Wrap-TTL': '30s'}});
-            process.env.VAULT_WRAPPED_SECRET_ID = wrapped_secret_id;
-        }
-        const vault = nodeVault({token: process.env.VAULT_WRAPPED_SECRET_ID, endpoint: process.env.VAULT_ADDR});
-        const {data: {secret_id}} = await vault.write('sys/wrapping/unwrap', {});
-        process.env.VAULT_SECRET_ID = secret_id;
-    } catch(error) {
-        console.error(error);
-        if(typeof error === 'object' && error !== null && 'response' in error) {
-            console.error(JSON.stringify((error as {response: any}).response));
-        }
-    }
+  try {
+    const vault = nodeVault({
+      token: await getWrappedToken(),
+      endpoint: process.env.VAULT_ADDR,
+    });
+    const {
+      data: { secret_id },
+    } = await vault.write("sys/wrapping/unwrap", {});
+    process.env.VAULT_SECRET_ID = secret_id;
+  } catch (error) {
+    const msg =
+      typeof error === "object" && error !== null && "response" in error
+        ? JSON.stringify((error as { response: any }).response)
+        : error;
+    console.error(msg);
+    process.exit(1);
+  }
 };
